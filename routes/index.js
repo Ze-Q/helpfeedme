ab712b4380fd0a3bec3a613d164896b95e576d16
Ze-Q <ze.q.zhang@mail.mcgill.ca>
2015-01-18 00:35:56 -0500

    Add the real index page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Main Page' });
+   res.sendfile(__dirname + '/public/index.html');
 });
 
 /* GET create user page */
